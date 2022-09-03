985f7a380c33fe13af26c11a5a05b729e245dd5d
Peter Chin <peterchinmm@gmail.com>
2022-09-03 21:52:56 +0800

    first commit

---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  all: [
+    {
+      id: 'what',
+      title: 'What is nuxt?',
+      content: 'this is nuxt'
+    },
+    {
+      id: 'why',
+      title: 'Why Nuxt?',
+      content: 'becuz deez nuxt aha gottem'
+    },
+    {
+      id: 'how',
+      title: 'How to nuxt?',
+      content: 'Please help me. I dont know either'
+    }
+  ]
+})
